22c27e5f1bf06ef0b9bc1dfa3c4ea01479f4d6fc
ArtisanForgeDesigns <artisanforgedesigns@gmail.com>
2025-09-14 15:00:07 -0500

    Update installer script

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 sudo apt update
 bash <(curl -L https://github.com/balena-io/wifi-connect/raw/master/scripts/raspbian-install.sh)
 sudo apt install python3 python3-pip python3-venv
-python3 -m venv myapp 
-source myapp/bin/activate
-pip install -r requirements.txt
 sudo chmod +x wifi-connect.sh run.sh
 
 # Add sudo crontab job
@@ -11,3 +8,7 @@ sudo chmod +x wifi-connect.sh run.sh
 
 # Add non-sudo crontab job
 (crontab -l 2>/dev/null; echo "@reboot /home/pilock/run.sh") | crontab -
+
+
+python3 -m venv myapp 
+./myapp/bin/pip install -r requirements.txt
