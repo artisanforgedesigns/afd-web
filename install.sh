afc10d8654cdaa2afa5ff57ce53ceaa56fbfb156
ArtisanForgeDesigns <artisanforgedesigns@gmail.com>
2025-09-14 15:45:04 -0500

    Update scripts

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo chmod +x wifi-connect.sh run.sh
 (sudo crontab -l 2>/dev/null; echo "@reboot sudo /home/pi/afd-web/wifi-connect.sh") | sudo crontab -
 
 # Add non-sudo crontab job
-(crontab -l 2>/dev/null; echo "@reboot /home/pi/afd-web/run.sh") | crontab -
+(crontab -l 2>/dev/null; echo "@reboot sleep 30 && /home/pi/afd-web/run.sh") | crontab -
 
 
 python3 -m venv myapp 
--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sleep 30
 cd /home/pi/afd-web/
 source myapp/bin/activate
 python3 app.py
